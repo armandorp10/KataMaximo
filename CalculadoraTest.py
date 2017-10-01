172d979b8bd26aeb4bff1732cb1da88e03459e3c
Diego Prens Ramos <armandorp10@gmail.com>
2017-09-30 20:19:07 -0500

    Paso 1 - Ciclo 2 Se agrega prueba para probar con un numero(rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,3 +5,6 @@ from Calculadora import Calculadora
 class CalculadoraTest(TestCase):
     def test_getEstadisticas(self):
         self.assertEquals(Calculadora().getEstadisticas(""), 0, "Cero numero de elementos")
+
+    def test_getEstadisticasUnNumero(self):
+        self.assertEquals(Calculadora().getEstadisticas("2"), 1, "Un elemento")
