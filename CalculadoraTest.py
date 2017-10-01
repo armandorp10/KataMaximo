8388ab5f3d0f307203c4c093de2d69a51b6497ff
Diego Prens Ramos <armandorp10@gmail.com>
2017-09-30 20:27:15 -0500

    Paso 1 - Ciclo 3 Se prueba con dos elementos en la cadena(Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -8,3 +8,6 @@ class CalculadoraTest(TestCase):
 
     def test_getEstadisticasUnNumero(self):
         self.assertEquals(Calculadora().getEstadisticas("2")[0], 1, "Un elemento")
+
+    def test_getEstadisticasDosNumeros(self):
+        self.assertEquals(Calculadora().getEstadisticas("2,3")[0], 2, "Dos elementos")
