c2e9757995830bf5c6b59ea46e79016daad4f0ff
LuisMesa <la.mesa10@uniandes.edu.co>
2017-09-30 21:21:55 -0500

    Paso 3 Ciclo 3 se modifica el codigo y se prueba para maximo con dos elementos (Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -5,6 +5,6 @@ class Calculadora:
             return [0, 0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros), int(min(numeros))]
+            return [len(numeros), int(min(numeros)), int(max(numeros))]
         else:
             return [1, int(cadena), int(cadena)]
