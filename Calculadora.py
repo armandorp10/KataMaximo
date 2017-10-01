788b059abb53eba4c0c16725c0aba952b0b04ce4
LuisMesa <la.mesa10@uniandes.edu.co>
2017-09-30 21:17:09 -0500

    Paso 3 Ciclo 1 se  modifica el codigo y se prueba para el maximo con ningún elemento (Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -2,7 +2,7 @@
 class Calculadora:
     def getEstadisticas(self, cadena):
         if cadena == "":
-            return [0, 0]
+            return [0, 0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros), int(min(numeros))]
